718385fd8335b5954ea8f45f864dd3537ab35de0
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-26 22:54:13 +0530

    Added css loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { run } from "./app/app";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 
+import "./main.css";
+
 const alertService = new AlertService();
 const componentService = new ComponentService();
 
